5228bbfec5e085fae2b44adef39b47e1db55ffed
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2024-04-27 16:21:18 +0530

    Implement OTP verification page
     - Add Source files for OTP verification page in forgot password section
    
    - Fixup packages.json file for 'reactjs-popup' module

--- kanteen/src/components/ForgotPwd.js
+++ kanteen/src/components/ForgotPwd.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function ForgotPwd() {
     return (
         <div>
             forgot pwd kiii
+            <Link to="/forgotpwdotp" className="signupacnt-loginBtn"><span >emailEntered</span></Link>
         </div>
     )
 }
