2bc6e8552ae573c28411e6f2031a2075df292049
abhiharsha96 <99648211+abhiharsha96@users.noreply.github.com>
2024-04-30 22:40:49 +0530

    added eyeicon in forgotpwd

--- kanteen/src/components/ForgotPwd.js
+++ kanteen/src/components/ForgotPwd.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/ForgotPwd.css';
 import forgotImg from '../images/forgotpwd.svg';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
 export default function ForgotPwd() {
+    const [checked, SetChecked] = useState(false);
+    const [email, SetEmail] = useState('');
+    const [emailValid, SetEmailValid] = useState(false);
+    const validateEmail = (email) => {
+        const regex = /@ch\.amrita\.edu$|@ch\.students\.amrita\.edu$/;
+        return regex.test(email);
+    }
+    const handleEmailChange = (e) => {
+        const { value } = e.target;
+        SetEmail(value);
+        SetEmailValid(validateEmail(value.toLowerCase()));
+    }
     return (
         <div className="forgotpwd">
             <div className="forgotpwd-forgot-container">
@@ -22,13 +37,36 @@ export default function ForgotPwd() {
                     </div>
                     <form className="forgotpwd-email">
                         <div className="forgotpwd-email-group">
-                            <label htmlFor="email"></label>
+                        <label
+                                htmlFor="email">
+                            </label>
                             <input
                                 type="email"
                                 name="email"
+                                onChange={handleEmailChange}
+                                value={email}
                                 id="email"
-                                placeholder="Enter your email "
+                                style={{outline:"none"}}
+                                className={emailValid ? "valid-border" :email.length>0? "invalid-border":""}
+                                placeholder="Enter your email address"
                                 required />
+                            {
+                                emailValid &&
+                                <FontAwesomeIcon className="validateicon" icon={faCircleCheck} beat style={{ color: "#139a72", }} />
+                            }
+                            {
+                                !emailValid && email.length > 0 &&
+                                <FontAwesomeIcon
+                                    className="validateicon"
+                                    icon={faCircleXmark}
+                                    shake
+                                    onClick={() => SetEmail('')}
+                                    style={{ color: "#ba1717", }}
+                                />
+                            }
+                        </div>
+                        <div className="email-check-alert">
+                            {!emailValid && email.length > 0 && <span>!! Enter a valid Amrita email address</span>}
                         </div>
                         <div className="forgotpwd-btn-container">
                             <button
