3d50144b8302cf091b1d6ba1cb41617556d74a22
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-05-08 03:15:53 +0530

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
