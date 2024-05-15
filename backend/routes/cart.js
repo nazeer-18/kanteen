59b950b37961e15e49cc9afa790b3098dbac218f
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-05-16 01:49:53 +0530

    functionality to fetch cart items of an user

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const cartRouter = express.Router();
 const Cart = require('../models/Cart')
+
+//Get all items from cart for a user
+cartRouter.get('/fetchall',async (req,res)=>{
+    try{
+        const userId = req.user_id;
+        const cart = await Cart.findOne({userId:userId}).populate('items.item');
+        res.status(200).json(cart);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+})
