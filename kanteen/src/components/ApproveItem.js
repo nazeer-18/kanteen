fa64a3de9d0efb6c74675b4c719a5089faa6351d
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-06-15 03:50:11 +0530

    added approve cash css and modifed order history css

--- kanteen/src/components/ApproveItem.js
+++ kanteen/src/components/ApproveItem.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import transactionService from '../services/transactionService';
+import '../styles/ApproveItem.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIndianRupee } from '@fortawesome/free-solid-svg-icons';
 
 export default function ApproveItem(props) {
     const { orderId, userId, total, expiry } = props;
+    const navigate = useNavigate();
     const createTransaction = async (status) => {
         try {
             const response = await transactionService.createTransaction(userId, orderId, "cash", status);
@@ -12,44 +17,52 @@ export default function ApproveItem(props) {
             console.error(err);
         }
     }
+    const viewOrder = () => {
+        navigate(`/vieworder?id=${orderId}`)
+    }
     return (
         <div className="approveitem-page">
+            <div className="approve-item-order-expiry">
+                {expiry} 04:00
+            </div>
             <div className="approve-item-order-details">
                 <div className="approve-item-order-id">
-                    Order id: {orderId}
+                    ID: {orderId} 123457894544
                 </div>
                 <div className="approve-item-user-id">
-                    User id: {userId}
+                    User: {userId} ch.en.u4cse21161@ch.students.amrita.edu
                 </div>
             </div>
-            <div className="approve-item-order-handling">
-                <button onClick={() => {
-                    createTransaction("success")
-                }
-                }>
-                    Approve
-                </button>
-                <button onClick={() => {
-                    createTransaction("failed")
-                }
-                }>
-                    Reject
-                </button>
-            </div>
+            <hr />
             <div className="approve-item-order-others">
-                <div className="approve-item-order-expiry">
-                    Expires in: {expiry}
-                </div>
                 <div className="approve-item-order-total">
-                    Total: {total}
+                    Total: <FontAwesomeIcon icon={faIndianRupee}/>  <span>{total}2000</span> 
                 </div>
                 <div className="approve-item-order-explore">
-                    <button>
-                        Explore
+                    <button onClick={viewOrder}>
+                        View order
                     </button>
                 </div>
             </div>
-
+            <hr />
+            <div className="approve-item-order-handling">
+                <div>
+                    <button onClick={() => {
+                        createTransaction("success")
+                    }
+                    }>
+                        Approve
+                    </button>
+                </div>
+                <div>
+                    <button onClick={() => {
+                        createTransaction("failed")
+                    }
+                    }>
+                        Reject
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
