d6503de0c1b65484a7e605419edf763dedd5b80a
Likhith karri <99127911+likhi-23@users.noreply.github.com>
2024-06-21 15:29:12 +0530

    add item admin

--- kanteen/src/components/AdditemAdmin.js
+++ kanteen/src/components/AdditemAdmin.js
@@ -1,34 +1,52 @@
 
 import React, { useState } from 'react';
+import itemService from '../services/itemService';
  import '../styles/AdditemAdmin.css';
 
 export default function AddItemAdmin() {
-  const [itemName, setItemName] = useState('');
+  const [name, setName] = useState('');
+  const [id, setId]=useState('');
   const [quantity, setQuantity] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
+  const [type, setType]=useState('');
+  const [image, setImage] = useState('');
   const [price, setPrice] = useState('');
   const [category, setCategory] = useState('');
 
   const categories = ['beverages', 'starters', 'main course', 'desserts', 'stationery', 'snacks', 'tiffin'];
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    const newItem = { itemName, quantity, imageUrl, price, category };
-    console.log('New Item:', newItem);
+    try{
+      const response = await itemService.addToMenu(id,name,price,quantity,image,type,category);
+      console.log(response.data)
+    }
+    catch(err){
+      console.log(err);
+    }
   };
 
   return (
     
-    <div className='cntnr'>
+    <div className='AdditemAdmin'>
+
         
       
       <form onSubmit={handleSubmit}>
+      <div>
+          <label>Id:</label>
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            required
+          />
+        </div>
         <div>
           <label>Item Name :</label>
           <input
             type="text"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
@@ -42,11 +60,26 @@ export default function AddItemAdmin() {
           />
         </div>
         <div>
+          <label>Type :</label>
+          <select
+            type="number"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            required
+            
+          ><option value="" disabled>Select Type</option>
+          
+            <option value='veg'>veg</option>
+            <option value='non-veg'>non-veg</option>
+            <option value='others'>others</option>
+          </select>
+        </div>
+        <div>
           <label>Image URL :</label>
           <input
             type="text"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
             required
           />
         </div>
